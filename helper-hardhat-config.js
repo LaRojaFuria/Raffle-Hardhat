9b889ad7e85ea4820168b8032e4ad79b3ee8f5bd
escarmical <escarmical@gmail.com>
2023-07-31 20:34:45 -1000

    Updated the interval to 3600

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -3,13 +3,13 @@ const { ethers } = require("hardhat")
 const networkConfig = {
     default: {
         name: "hardhat",
-        keepersUpdateInterval: "30",
+        keepersUpdateInterval: "3600",
     },
     31337: {
         name: "localhost",
         subscriptionId: "588",
         gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
-        keepersUpdateInterval: "30",
+        keepersUpdateInterval: "3600",
         raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
         callbackGasLimit: "500000", // 500,000 gas
     },
@@ -17,14 +17,23 @@ const networkConfig = {
         name: "sepolia",
         subscriptionId: "6926",
         gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
-        keepersUpdateInterval: "30",
+        keepersUpdateInterval: "3600",
         raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
         callbackGasLimit: "500000", // 500,000 gas
         vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
     },
     1: {
         name: "mainnet",
-        keepersUpdateInterval: "30",
+        keepersUpdateInterval: "3600",
+    },
+    137: {
+        name: "polygon",
+        subscriptionId: "",
+        gasLane: "0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c", // 30 gwei
+        keepersUpdateInterval: "3600",
+        raffleEntranceFee: ethers.utils.parseEther("0.01"), // 0.01 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+        vrfCoordinatorV2: "0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625",
     },
 }
 
